ba9f846133a7e9ece59bf092da4064580f734843
Nicky Lam <71570655+nkylm@users.noreply.github.com>
2024-11-05 14:40:44 -0500

    commit

---
+++ soundProcessing.py
@@ -0,0 +1,47 @@
+import pyaudio
+import wave
+mysp=__import__("my-voice-analysis")
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 10
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+p="output" # Audio File title
+c=r"C:\\Users\Shun\Documents\\4A\\CAPSTONE\\VocoPal" # Path to the Audio_File directory (Python 3.7)
+mysp.mysppaus(p,c)
+
+
+
+
