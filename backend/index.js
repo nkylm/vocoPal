50fb2ca3673c5e4960b3eb13c51888f00cdbb512
David Liu <d282liu@uwaterloo.ca>
2025-03-18 16:06:12 -0400

    changed process env to hosted urls for testing

--- backend/index.js
+++ backend/index.js
@@ -193,7 +193,7 @@ app.post("/api/upload", upload.single("audio"), async (req, res) => {
     console.log("userThresholds: ", userThresholds);
 
     // Send the file to the Python microservice
-    const microserviceUrl = process.env.FLASK_URL;
+    const microserviceUrl = process.env.FLASK_HOSTED_URL;
     const formData = new FormData();
     formData.append("audio", fs.createReadStream(audioFilePath));
 
@@ -263,11 +263,11 @@ app.post("/api/upload", upload.single("audio"), async (req, res) => {
 
     console.log(
       "process.env.BACKEND_HOSTED_URL: ",
-      process.env.BACKEND_URL,
+      process.env.BACKEND_HOSTED_URL,
     );
 
     const speechDataResponse = await axios.post(
-      `${process.env.BACKEND_URL}/api/speechData`,
+      `${process.env.BACKEND_HOSTED_URL}/api/speechData`,
       speechDataPayload,
     );
 
